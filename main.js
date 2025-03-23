17d255c11b408bb34a1c942712453f5da9694dec
Wuildermoon <wuilderman2000@gmail.com>
2025-03-22 19:20:21 -0500

    alura's barbery v1.1

--- main.js
+++
@@ -1,9 +0,0 @@
-const itemList = document.querySelectorAll(".list-item");
-const button = document.querySelector("#button");
-
-button.addEventListener("click", () => {
-  itemList.forEach((item) => {
-    item.style.color = "red";
-    item.style.fontSize = "40px";
-  });
-});
